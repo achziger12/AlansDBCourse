da58c95e6f25452b9450d85d27f6b3ca3e1ff2d3
achziger12 <Edward.Achziger1@marist.edu>
2016-02-24 10:15:43 -0500

    Updated Lab 5
    Still having trouble with problem 7

--- Lab5.sql
+++ Lab5.sql
@@ -39,8 +39,8 @@ Where customers.cid NOT IN (select orders.cid
 --4. Show	the	names	of	customers	who	have	never	placed	an	order.	Use	an	outer	join.	
 
 Select distinct customers.name
-From customers LEFT JOIN orders
-On customers.cid = orders.cid;
+From customers LEFT JOIN orders On customers.cid = orders.cid
+Where orders.cid is NULL;
 
 
 
@@ -48,22 +48,6 @@ On customers.cid = orders.cid;
 
 --5. Show	the	names	of	customers	who	placed	at	least	one	order	through	an	agent	in	their	
 --own	city,	along	with	those	agent(s')	names.	
-
-Select customers.name, agents.name, SUM(orders.ordnum)
-From customers, orders, agents
-Where customers.cid = orders.cid  AND
-      agents.aid = orders.aid  AND
-      agents.city = customers.city AND
-      orders.ordnum > 1
-  Group by orders.ordnum, customers.name, agents.name;
-
-
-
-
---6. Show	the	names	of	customers	and	agents	living	in	the	same	city,	along	with	the	name	
---of	the	shared	city,	regardless	of	whether	or	not	the	customer	has	ever	placed	an	order	
---with	that	agent.	
-
 
 Select customers.name, agents.name, orders.ordnum
 From customers, orders, agents
@@ -73,9 +57,44 @@ Where customers.cid = orders.cid  AND
 
 
 
+--6. Show	the	names	of	customers	and	agents	living	in	the	same	city,	along	with	the	name	
+--of	the	shared	city,	regardless	of	whether	or	not	the	customer	has	ever	placed	an	order	
+--with	that	agent.	
+
+
+Select customers.name
+From customers
+Where customers.city IN (select city
+			From ag_cust_city)
+Union
+Select agents.name
+From agents
+Where agents.city IN (select city
+			From ag_cust_city);	       
+
+--The most complex query you will ever see for this problem 
+-- The view I used below
+
+Create view ag_cust_city
+as      
+Select customers.city
+From customers
+Intersect
+Select agents.city
+From agents;
+
+
+
 
 --7. Show	the	name	and	city	of	customers	who	live	in	the	city	that	makes	the	fewest
 --different	kinds	of	products.	(Hint:	Use	count	and	group	by	on	the	Products	table.)
 
-
+Select customers.name, customers.city
+From customers, products, orders
+Where customers.cid = orders.cid AND
+      products.pid = orders.pid AND
+      customers.city IN ( select products.city
+			  From products
+			  Where quantity = 1
+			  Order by count(quantity))
 
